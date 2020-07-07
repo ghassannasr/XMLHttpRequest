d8f72e07ba42a28c26bf5fad407d37f78248fc8e
Ghassan Nasr <ghassan.nasr@gmail.com>
2020-07-07 18:06:55 -0400

    see the commented out code and my replacement

--- js/configureXmlHttpRequest.js
+++ js/configureXmlHttpRequest.js
@@ -1,36 +1,120 @@
 let button = document.getElementById("button");
-button.onclick = makeAsynchronousRequestToWebserver("https://www.msn.com/", updateDom);
+button.onclick = makeAsynchronousRequestToWebserver;
 
-function makeAsynchronousRequestToWebserver(url, callback) { // can pass in more args
-  let request = new XMLHttpRequest();
-  request.callback = callback;
-  //request.arguments = Array.prototype.slice.call(arguments, 2);
-  request.onload = function() {
-    this.callback.apply(this, this.arguments); 
-  }
-  request.onerror = function() { 
-    console.error(this.statusText); 
-  };
-  request.open("GET", url, true);
-  request.send(null);
+
+
+function createXmlHttpRequest() {
+    let request;
+    try { // Opera 8.0+, Firefox, Safari
+        request = new XMLHttpRequest();
+    } catch(someError) {
+        try { // Internet Explorer Browser
+            request = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch(someError) {
+            try {
+                request = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch(someError) {
+                alert("Download a better browser");
+            }
+        }
+    }
+    return request;
 }
 
 
-function updateDom(request) {
-    let someValue1;
+function configureXmlHttpRequest(request) {
+    request.onreadystatechange = function() {
+        // request finished and response is ready
+        if(request.readyState == 4) {
+            updateDom(request);
+        }
+    }
+    console.log("onreadystatechange calls the callback() function is = " + request.onreadystatechange)
+    console.log("The readyState at this point is = " + request.readyState);
+}
+
+
+class Response {
+    constructor(id, authToken, body) {
+        this.id =id;
+        this.authToken = authToken;
+        this.body = body;
+    }
+}
+
+class Person {
+    constructor(fname, lname, birthdate) {
+        this.fname = fname;
+        this.lname = lname;
+        this.birthdate = birthdate;
+    }
+}
+
+let leon1 = new Person("Leon", "Hunter", "01/01/01");
+let leon2 = {
+    fname: "leon",
+    lname: "hunter",
+    birthdate: "01/01/01"
+};
+let leon3 = new Person(leon2);
+
+
+let haseeb1 = new Person("Muhammad", "Haseeb", "01/01/01");
+let haseeb2 = {
+    fname: "Muhammad",
+    lname: "Haseeb",
+    birthdate: "01/01/01"
+};
+
+
+
+//function updateDom(response) {
+  function updateDom(request) {
+    document.write(request.response);
+    /*
+    let exepctedResponseBody = {
+        id: 0,
+        authToken: "83902312809adjldasjkl#$%^&adshjk13DBGAHSJ",
+        body: {
+            fname: "leon",
+            lname: "hunter",
+            birthdate: "01/01/01"
+        }
+    };
+
     let someValue2;
     let someValue3;
-    if(request == someValue1) {
+    if(response== exepctedResponseBody) {
         document.writeln("manipulation 1")
     }
 
-    if(request == someValue2) {
+    if(response == someValue2) {
         document.writeln("manipulation 2")
     }
 
-    if(request == someValue3) {
+    if(response == someValue3) {
         document.writeln("manipulation 2")
     }
+    */
 }
 
 
+function makeAsynchronousRequestToWebserver() {
+    let request = createXmlHttpRequest();
+    configureXmlHttpRequest(request);
+    request.open("GET", "https://www.msn.com/", false);
+    //let response = request.send(null);
+    request.send(null);
+    console.log(request.response);
+    //console.log(response);
+}
+
+/*
+async function makeAsynchronousRequestToWebserver() {
+    let request = await createXmlHttpRequest();
+    configureXmlHttpRequest(request);
+    request.open("GET", "https://www.msn.com/" );
+    let response = request.send(null);
+    console.log(response);
+}
+*/
